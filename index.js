ff6140a5ef179e9849c446bda63120d92ebca50c
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2021-09-30 20:04:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const check = document.querySelector("#checkbtn")
+
+check.addEventListener("click", checkNumberIsLucky);
+
+//Sorry for the comments but it is for my knowledge , so that I can easily go through this code anytime without getting into any truble
+function checkNumberIsLucky() {
+    // console.log(dateOfBirth.value);
+    // console.log(luckyNumber.value)
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    
+    // console.log(dob);
+    // console.log(sum);
+    const result = luckyornot(sum , luckyNumber)
+    
+
+
+}
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-" , "" );
+    let sum = 0;
+    for (let index = 0; index < dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
+function luckyornot(sum , luckyNumber){
+    const luckynumber = Number(luckyNumber.value);
+
+    if ((sum%luckynumber) == 0){
+        document.getElementById("output").innerHTML = "Yes , It is lucky"
+    }
+    else{
+        document.getElementById("output").innerHTML = "Sorry , It is not lucky "
+    }
+}
